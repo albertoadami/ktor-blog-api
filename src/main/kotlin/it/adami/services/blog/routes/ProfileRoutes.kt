972bd3e108d7050b85d7884ce0e0fa75e2ab964f
alberto adami <alberto.adami.7@gmail.com>
2024-11-27 14:01:37 +0100

    feature: Add GET /profile endpoint (#14)

---
+++ src/main/kotlin/it/adami/services/blog/routes/ProfileRoutes.kt
@@ -0,0 +1,41 @@
+package it.adami.services.blog.routes
+
+import io.ktor.http.*
+import io.ktor.server.auth.*
+import io.ktor.server.auth.jwt.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import it.adami.services.blog.authentication.Authentication
+import it.adami.services.blog.converter.toJson
+import it.adami.services.blog.service.UserService
+
+class ProfileRoutes(private val userService: UserService, private val authentication: Authentication) {
+
+    private fun Route.routes() {
+        route("profile") {
+            authenticate("auth-jwt") {
+                get {
+                    try {
+                        val principal = call.principal<JWTPrincipal>()
+                            ?: return@get call.respond(HttpStatusCode.Unauthorized)
+
+                        val token = authentication.extractTokenInfo(principal)
+                            ?: return@get call.respond(HttpStatusCode.Unauthorized)
+
+                        val user = userService.getById(token.userId)
+                            ?: return@get call.respond(HttpStatusCode.NotFound)
+
+                        call.respond(user.toJson())
+                    } catch (e: Exception) {
+                        call.respond(HttpStatusCode.InternalServerError)
+                    }
+                }
+            }
+        }
+    }
+
+    fun register(route: Route) {
+        route.apply { routes() }
+    }
+
+}
